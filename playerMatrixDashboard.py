d2b22cb4b59eb75241968ba4fa2330df5061dea1
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-08 16:13:47 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -8,7 +8,11 @@ import streamlit as st
 import pandas as pd
 import runSims as sims
 
+#instantiate
 st.title('Optimal PLayer Degrees of Seperation')
+#initial data
+df = pd.DataFrame()
+sim_results = pd.DataFrame()
 
 #get data from upload
 dat = st.sidebar.file_uploader('Upload CSV Player Data Here')
@@ -18,12 +22,15 @@ upload_button = st.sidebar.button('Data Uploaded')
 if upload_button:
     df = pd.read_excel(dat)
     #show data
-    with st.expander('Uploaded Data'):
-        st.dataframe(df)
+with st.expander('Uploaded Data'):
+    st.dataframe(df)
 
-    count = st.sidebar.number_input('How many sims to run?')
-    sims_button = st.sidebar.button('Run Sims')
-    if sims_button:
-        sim_results = sims.standard_sims(df, 'nfl', count, fpts_col_name='avg fpts', ceil_column = 'avg ceil', floor_column = 'avg floor', include_correlations=True)        
-        with st.expander('Sims Results'):
-            st.dataframe(sim_results)
+
+count = st.sidebar.number_input('How many sims to run?')
+sims_button = st.sidebar.button('Run Sims')
+if sims_button:
+    sim_results = sims.standard_sims(df, 'nfl', count, fpts_col_name='avg fpts', ceil_column = 'avg ceil', floor_column = 'avg floor', include_correlations=True)        
+    
+    
+with st.expander('Sims Results'):
+    st.dataframe(sim_results)
