bb94b177a96adbac460cdab45341d948c8efca70
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-08 15:37:30 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -8,4 +8,8 @@ import streamlit as st
 import pandas as pd
 import runSims as sims
 
+st.title('Optimal PLayer Degrees of Seperation')
+
 #get data from upload
+dat = st.sidebar.file_uploader('Upload CSV Player Data Here')
+df = pd.read_csv(dat)
