9b39aca6849e790d29282be5e6c021279296f92f
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-08 16:11:30 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -20,3 +20,10 @@ if upload_button:
     #show data
     with st.expander('Uploaded Data'):
         st.dataframe(df)
+
+    count = st.sidebar.number_input('How many sims to run?')
+    sims_button = st.sidebar.button('Run Sims')
+    if sims_button:
+        sim_results = sims.standard_sims(df, 'nfl', count, fpts_col_name='avg fpts', ceil_column = 'avg ceil', floor_column = 'avg floor', include_correlations=True)        
+        with st.expander('Sims Results'):
+            st.dataframe(sim_results)
