ffa29415ae00b6bc8191a8e077f83cbd574e6fb5
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-09 16:29:24 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -7,6 +7,7 @@ Created on Sun Jan  8 15:28:03 2023
 import streamlit as st
 import pandas as pd
 import runSims as sims
+import playerMatrixDashboardController as controller
 
 #instantiate
 st.title('Optimal PLayer Degrees of Seperation')
@@ -14,15 +15,19 @@ st.title('Optimal PLayer Degrees of Seperation')
 st.session_state.key = 'sim results'
 st.session_state.key = 'input data'
 st.session_state.key = 'lineups'
+st.session_state.key = 'player matches'
+st.session_state.key = 'relationships data'
 st.session_state['sim results '] = pd.DataFrame()
 st.session_state['input data']  = pd.DataFrame()
+st.session_state['relationships data'] = pd.DataFrame()
 
 #initial tab layout
-data_tab, sims_tab, explore_tab = st.tabs(['Import Projections','Run Sims', 'Explore Relationships'])
+data_tab, sims_tab, relationships_tab = st.tabs(['Import Projections','Run Sims', 'Explore Relationships'])
 
 
 #get data from upload
-dat = st.sidebar.file_uploader('Upload CSV Player Data Here')
+with data_tab:
+    dat = st.file_uploader('Upload CSV Player Data Here')
 
 if dat is not None:
     df = pd.read_excel(dat)
@@ -30,18 +35,24 @@ if dat is not None:
     with data_tab:
         st.dataframe(st.session_state['input data'])
     
-
+#sims tab
 with sims_tab:    
-    count = int(st.sidebar.number_input('How many sims to run?'))    
-    sims_button = st.sidebar.button('Run Sims')
+    count = int(st.number_input('How many sims to run?'))    
+    sims_button = st.button('Run Sims')
+    st.subheader('Sims Results')
+    st.dataframe(st.session_state['sim results'])
     
 if sims_button:
     sims_results,lineups = sims.standard_sims(df, 'nfl', count, fpts_col_name='avg fpts', ceil_column = 'avg ceil', floor_column = 'avg floor', include_correlations=True) 
     st.session_state['sim results'] = sims_results
     st.session_state['lineups'] = lineups
     
-                
-with sims_tab:
-    st.subheader('Sims Results')
-    st.dataframe(st.session_state['sim results'])
+#relationships tab
+with relationships_tab:
+    st.dataframe(st.session_state['relationships data'])
+    player_options = list(st.session_state['sim results'].index)
+    selected_players = set(st.multiselect('Players to Include', options = player_options))
+    st.session_state['relationships_data'] = controller.lineup_parser(st.session_state['lineups'], selected_players)
+    
+             
     
