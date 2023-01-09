e4ebf0ce286f4e080a519d680a8929df6168610d
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-09 15:34:56 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -10,12 +10,15 @@ import runSims as sims
 
 #instantiate
 st.title('Optimal PLayer Degrees of Seperation')
-#initial data
+#initial data caching
 st.session_state.key = 'sim results'
 st.session_state.key = 'input data'
 st.session_state.key = 'lineups'
 st.session_state['sim results '] = pd.DataFrame()
 st.session_state['input data']  = pd.DataFrame()
+
+#initial tab layout
+data_tab, sims_tab, explore_tab = st.tabs(['Import Projections','Run Sims', 'Explore Relationships'])
 
 
 #get data from upload
@@ -24,17 +27,21 @@ dat = st.sidebar.file_uploader('Upload CSV Player Data Here')
 if dat is not None:
     df = pd.read_excel(dat)
     st.session_state['input data'] = df
-    with st.expander('Uploaded Data'):
+    with data_tab:
         st.dataframe(st.session_state['input data'])
     
+
+with sims_tab:    
+    count = int(st.sidebar.number_input('How many sims to run?'))    
+    sims_button = st.sidebar.button('Run Sims')
     
-count = int(st.sidebar.number_input('How many sims to run?'))    
-sims_button = st.sidebar.button('Run Sims')
 if sims_button:
     sims_results,lineups = sims.standard_sims(df, 'nfl', count, fpts_col_name='avg fpts', ceil_column = 'avg ceil', floor_column = 'avg floor', include_correlations=True) 
     st.session_state['sim results'] = sims_results
     st.session_state['lineups'] = lineups
     
                 
-with st.expander('Sims Results'):
+with sims_tab:
+    st.subheader('Sims Results')
     st.dataframe(st.session_state['sim results'])
+    
