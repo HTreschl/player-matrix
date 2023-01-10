5491720b8cb4ebb5fac79e56c70863937809a5fb
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-09 20:47:16 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -10,7 +10,8 @@ import runSims as sims
 import playerMatrixDashboardController as controller
 
 #instantiate
-st.title('Optimal PLayer Degrees of Seperation')
+st.title('DFS Degrees of Separation')
+sample_data = pd.read_csv('Sample App Data.csv')
 #initial data caching
 st.session_state.key = 'sim results'
 st.session_state.key = 'input data'
@@ -20,12 +21,32 @@ st.session_state.key = 'relationships data'
 st.session_state.key = 'sim count'
 
 #initial tab layout
-data_tab, sims_tab, relationships_tab = st.tabs(['Import Projections','Run Sims', 'Explore Relationships'])
+intro_tab, data_tab, sims_tab, relationships_tab = st.tabs(['What is this?','Import Projections','Run Sims', 'Explore Relationships'])
 
+#write documentation
+with intro_tab:
+    st.header('FAQ')
+    with st.expander('What does this tool do?'):
+        st.write('''The degrees of separation tool allows you to run game simulations using your own player projections and quickly visualize the frequency with which players appear together.
+                 For example, if you are building a stack with Allen, you might want to know whether the sims find a 1,2, or 3 receiver stack most frequently in the optimal lineups.
+                 The tool also includes the ability to run sims for all players and determine the optimal frequency a player will appear.
+                 ''')
+    with st.expander('How can this help me?'):
+        st.write('''We all know stacking teams and maximizing within-lineup correlation is key to profitably playing DFS. However, there is also a tradeoff for correlation in lineup ceiling. 
+                 Let's say you want Mahomes and Kelce in a lineup. It might be tempting to include Smith-Schuster for the correlation, but he might not be the highest projected receiver at
+                 that pricepoint. This tool runs the sims and presents the tradeoff between correlation and ceiling.
+                 ''')
+    with st.expander('How do I use it?'):
+        st.write('''For starters, you'll need to upload a csv file with your player projections in the "Import Projections" tab. A schematic sheet is 
+                 available for download for example formatting (WIP). Once you've uploaded projections, you can run sims and get the mathematically optimal play rate for each player. 
+                 Once you've run the sims, dive into the data in the "explore Relationships" section.
+                 ''')
+                 
 
 #get data from upload
 with data_tab:
     dat = st.file_uploader('Upload CSV Player Data Here')
+    st.download_button('Download a Template', data = sample_data.to_csv().encode('utf-8'), file_name = 'Sample DFS Data.csv')
 
 if dat is not None:
     df = pd.read_excel(dat)
@@ -34,12 +55,13 @@ if dat is not None:
         st.dataframe(st.session_state['input data'])
     
 #sims tab
-with sims_tab:    
+with sims_tab:
+    st.subheader('Sims Settings')    
     count = int(st.number_input('How many sims to run?'))    
     sims_button = st.button('Run Sims')
     
 if sims_button:
-    sims_results,lineups = sims.standard_sims(df, 'nfl', count, fpts_col_name='avg fpts', ceil_column = 'avg ceil', floor_column = 'avg floor', include_correlations=True)
+    sims_results,lineups = sims.standard_sims(df, 'nfl', count, fpts_col_name='fpts', ceil_column = 'ceil', floor_column = 'floor', include_correlations=True)
     st.session_state['sim results'] = sims_results
     st.session_state['lineups'] = lineups
     st.session_state['sim count'] = count
@@ -56,6 +78,7 @@ with relationships_tab:
     player_options = list(st.session_state['sim results'].index)
     selected_players = set(st.multiselect('Players to Include', options = player_options))
     if len(selected_players) > 0:
+        st.subheader('Optimal Pairings for {}'.format(', '.join(selected_players)))
         filtered_players = controller.lineup_parser(st.session_state['lineups'], selected_players)
         tot_matching_lineups = list(filtered_players.loc[filtered_players['Name']==list(selected_players)[0], ['Count']]['Count'])[0]
         filtered_players = filtered_players[filtered_players['Name'].isin(selected_players)==False]
