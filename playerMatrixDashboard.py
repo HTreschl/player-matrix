80b8aeaf788c51e12250d35d90bca742b573de19
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-01-10 19:40:57 -0600

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -10,8 +10,7 @@ import runSims as sims
 import playerMatrixDashboardController as controller
 
 #instantiate
-st.title('DFS Degrees of Separation')
-sample_data = pd.read_csv('Sample App Data.csv')
+sample_data = pd.read_csv('Sample App Data.csv') #upload sample data
 #initial data caching
 st.session_state.key = 'sim results'
 st.session_state.key = 'input data'
@@ -20,8 +19,16 @@ st.session_state.key = 'player matches'
 st.session_state.key = 'relationships data'
 st.session_state.key = 'sim count'
 
-#initial tab layout
+#set global constants
+if st.session_state['input data'] is not None:
+    has_valid_data = controller.data_checker(st.session_state['input data'])
+else:
+    has_valid_data=False
+
+#initial tab layout and title
+st.title('DFS Degrees of Separation')
 intro_tab, data_tab, sims_tab, relationships_tab = st.tabs(['What is this?','Import Projections','Run Sims', 'Explore Relationships'])
+
 
 #write documentation
 with intro_tab:
@@ -43,57 +50,72 @@ with intro_tab:
                  ''')
                  
 
-#get data from upload
+#get data from upload, data sample, and example download
 with data_tab:
     dat = st.file_uploader('Upload CSV Player Data Here')
     col1,col2 = st.columns(2)
+    
     with col1:
         st.download_button('Download a Template', data = sample_data.to_csv().encode('utf-8'), file_name = 'Sample DFS Data.csv')
+        st.caption('Replace column values with your own projections. Ceiling, floor, and ownership columns are optional.')
     with col2:
         sample_button = st.button('Use Sample Data')
 
 if sample_button:
     st.session_state['input data'] = sample_data
 
-if dat is not None or st.session_state['input data'] is not None: #show the data if it's available
-    if dat is not None:
-        df = pd.read_csv(dat)
-        st.session_state['input data'] = df
+if dat is not None:
+    df = pd.read_csv(dat)
+    st.session_state['input data'] = df   
+    has_valid_data = controller.data_checker(st.session_state['input_data'])
+
+#if valid data exists
+if has_valid_data:
     with data_tab:
         st.subheader('Imported Data')
         st.dataframe(st.session_state['input data'])
     
-#sims tab
-with sims_tab:
-    st.subheader('Sims Settings')    
-    count = int(st.number_input('How many sims to run?'))    
-    sims_button = st.button('Run Sims')
-    
-if sims_button: #run the sims
-    sims_results,lineups = sims.standard_sims(st.session_state['input data'], 'nfl', count, fpts_col_name='Fpts', ceil_column = 'Ceil', floor_column = 'Floor', include_correlations=True)
-    st.session_state['sim results'] = sims_results
-    st.session_state['lineups'] = lineups
-    st.session_state['sim count'] = count
+    #sims tab
     with sims_tab:
-        st.subheader('Sims Results')
-        st.dataframe(st.session_state['sim results'])
-else: #persist the results table
+        st.subheader('Sims Settings')    
+        count = int(st.number_input('How many sims to run?'))    
+        sims_button = st.button('Run Sims')
+        
+    if sims_button: #run the sims
+        sims_results,lineups = sims.standard_sims(st.session_state['input data'], 'nfl', count, fpts_col_name='Fpts', ceil_column = 'Ceil', floor_column = 'Floor', include_correlations=True)
+        st.session_state['sim results'] = sims_results
+        st.session_state['lineups'] = lineups
+        st.session_state['sim count'] = count
+        with sims_tab:
+            st.subheader('Sims Results')
+            st.dataframe(st.session_state['sim results'])
+    else: #persist the results table
+        with sims_tab:
+            st.subheader('Sims Results')
+            st.dataframe(st.session_state['sim results']) 
+        
+    #relationships tab
+    with relationships_tab:
+        player_options = list(st.session_state['sim results'].index)
+        selected_players = set(st.multiselect('Players to Include', options = player_options))
+        if len(selected_players) > 0:
+            st.subheader('Optimal Pairings for {}'.format(', '.join(selected_players)))
+            filtered_players = controller.lineup_parser(st.session_state['lineups'], selected_players)
+            tot_matching_lineups = list(filtered_players.loc[filtered_players['Name']==list(selected_players)[0], ['Count']]['Count'])[0]
+            filtered_players = filtered_players[filtered_players['Name'].isin(selected_players)==False]
+            filtered_players['% of Filtered Lineups'] = (filtered_players['Count']/tot_matching_lineups)*100
+            filtered_players['% of Total Lineups'] = (filtered_players['Count']/st.session_state['sim count'])*100
+            filtered_players = filtered_players.set_index('Name')
+            st.session_state['relationships data'] = filtered_players
+            st.dataframe(st.session_state['relationships data'])   
+            
+else: #no valid data
+    with data_tab:
+        st.subheader('Input data not available or format is incorrect. Upload data or confirm column names match the template')
     with sims_tab:
-        st.subheader('Sims Results')
-        st.dataframe(st.session_state['sim results']) 
+        st.subheader('Upload data to run sims')
+    with relationships_tab: 
+        st.subheader('Upload data and run sims to see player relationships')
     
-#relationships tab
-with relationships_tab:
-    player_options = list(st.session_state['sim results'].index)
-    selected_players = set(st.multiselect('Players to Include', options = player_options))
-    if len(selected_players) > 0:
-        st.subheader('Optimal Pairings for {}'.format(', '.join(selected_players)))
-        filtered_players = controller.lineup_parser(st.session_state['lineups'], selected_players)
-        tot_matching_lineups = list(filtered_players.loc[filtered_players['Name']==list(selected_players)[0], ['Count']]['Count'])[0]
-        filtered_players = filtered_players[filtered_players['Name'].isin(selected_players)==False]
-        filtered_players['% of Filtered Lineups'] = (filtered_players['Count']/tot_matching_lineups)*100
-        filtered_players['% of Total Lineups'] = (filtered_players['Count']/st.session_state['sim count'])*100
-        filtered_players = filtered_players.set_index('Name')
-        st.session_state['relationships data'] = filtered_players
-        st.dataframe(st.session_state['relationships data'])      
+        
     
