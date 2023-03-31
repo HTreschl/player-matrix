42684784919ab0ff64c0cecee718db608ec4f226
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-03-31 15:10:53 -0500

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -87,10 +87,10 @@ if st.session_state['sport'] != '':
     if has_valid_data:
         with data_tab:
             st.subheader('Imported Data')
-            st.session_state['included_teams'] = st.multiselect('Teams To Include',team_options, default = st.session_state['included_teams']) 
-            df = st.session_state['input data']
-            df = df[df['Team'].isin(st.session_state['included_teams'])]
-            st.session_state['input data'] = df
+            #st.session_state['included_teams'] = st.multiselect('Teams To Include',team_options, default = st.session_state['included_teams']) 
+            #df = st.session_state['input data']
+            #df = df[df['Team'].isin(st.session_state['included_teams'])]
+            #st.session_state['input data'] = df
             st.dataframe(st.session_state['input data'])
         
         #sims tab
