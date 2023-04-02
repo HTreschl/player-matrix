cb3e501f21016ea92290f56f07e3e4534addc98f
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-04-02 10:45:05 -0500

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -36,6 +36,8 @@ if 'sport_class' not in st.session_state:
     st.session_state['sport_class'] = None
 if 'included_teams' not in st.session_state:
     st.session_state['included_teams'] = []
+if 'stack_data' not in st.session_state:
+    st.session_state['stack_data'] = pd.DataFrame()
 
 #set global constants
 has_valid_data = controller.data_checker(st.session_state['input data'],st.session_state['sport'])
@@ -45,17 +47,117 @@ has_valid_data = controller.data_checker(st.session_state['input data'],st.sessi
 st.title('RosterTech')
 st.session_state['sport'] = st.selectbox('Select Sport', ['','NFL', 'MLB'])
 
+#%%MLB
+if st.session_state['sport'] == 'MLB':
+    intro_tab, data_tab, sims_tab, relationships_tab, stacks_tab = st.tabs(['What is this?','Import Projections','Run Sims', 'Explore Relationships', 'Stacks'])
+    
+    #start appropriate class
+    sport_class = sims.mlb()
+    st.session_state['correlation dict'] = {'SP':{}}
+    
+    #write documentation
+    with intro_tab:
+        intro_container = st.container()
+                     
+    
+    #get data from upload, data sample, and example download
+    with data_tab:
+        dat = st.file_uploader('Upload CSV Player Data Here')
+        col1,col2 = st.columns(2)
+        
+        with col1:
+            st.download_button('Download a Template', data = sample_data.to_csv(index=False).encode('utf-8'), file_name = 'Sample DFS Data.csv')
+            st.caption('Replace column values with your own projections. Ownnership column is optional.')
+        with col2:
+            sample_button = st.button('Use Sample Data')
+    
+    if sample_button:
+        st.session_state['input data'] = sample_data
+        has_valid_data = controller.data_checker(st.session_state['input data'], st.session_state['sport'])
+    
+    if dat is not None:
+        df = pd.read_csv(dat)
+        st.session_state['input data'] = df   
+        has_valid_data = controller.data_checker(st.session_state['input data'], st.session_state['sport'])
+        team_options = set(df['Team'])
+        
+    #if valid data exists
+    if has_valid_data:
+        with data_tab:
+            st.subheader('Imported Data')
+            #st.session_state['included_teams'] = st.multiselect('Teams To Include',team_options, default = st.session_state['included_teams']) 
+            #df = st.session_state['input data']
+            #df = df[df['Team'].isin(st.session_state['included_teams'])]
+            #st.session_state['input data'] = df
+            st.dataframe(st.session_state['input data'])
+        
+        #sims tab
+        with sims_tab:
+            st.subheader('Sims Settings')    
+            count = int(st.number_input('How many sims to run?'))    
+            sims_button = st.button('Run Sims')
+            
+        if sims_button: #run the sims
+            status_bar = st.progress(0)
+            if 'Ownership' in list(st.session_state['input data'].columns):
+                ownership_col = 'Ownership'
+            else:
+                ownership_col = None
+                
+            sims_results,lineups = sport_class.standard_sims(st.session_state['input data'], count,correlation_values = st.session_state['correlation dict'], fpts_col_name='Fpts', ceil_column = 'Ceil', floor_column = 'Floor',ownership_column=ownership_col, status_bar=status_bar)
+            status_bar.empty()
+            st.session_state['sim results'] = sims_results
+            st.session_state['lineups'] = lineups
+            st.session_state['sim count'] = count
+            with sims_tab:
+                st.subheader('Sims Results')
+                st.dataframe(st.session_state['sim results'])
+                st.download_button('Download Sims Results', data = st.session_state['sim results'].to_csv().encode('utf-8'), file_name = 'Sim Results.csv')
+        else: #persist the results table
+            with sims_tab:
+                st.subheader('Sims Results')
+                st.dataframe(st.session_state['sim results'])
+                st.download_button('Download Sims Results', data = st.session_state['sim results'].to_csv().encode('utf-8'), file_name = 'Sim Results.csv')
+            
+        #relationships tab
+        with relationships_tab:
+            player_options = list(st.session_state['sim results'].index)
+            selected_players = set(st.multiselect('Players to Include', options = player_options))
+            if len(selected_players) > 0:
+                st.subheader('Optimal Pairings for {}'.format(', '.join(selected_players)))
+                filtered_players = controller.lineup_parser(st.session_state['lineups'], selected_players)
+                tot_matching_lineups = list(filtered_players.loc[filtered_players['Name']==list(selected_players)[0], ['Count']]['Count'])[0]
+                filtered_players = filtered_players[filtered_players['Name'].isin(selected_players)==False]
+                filtered_players['% of Filtered Lineups'] = (filtered_players['Count']/tot_matching_lineups)*100
+                filtered_players['% of Total Lineups'] = (filtered_players['Count']/st.session_state['sim count'])*100
+                filtered_players = filtered_players.set_index('Name')
+                st.session_state['relationships data'] = filtered_players
+                st.dataframe(st.session_state['relationships data'])
+                
+        with stacks_tab:
+            stack_size = st.slider('Stack Size', min_value = 1, max_value = 6, step = 1)
+            stack_feature = st.selectbox('Stack Feature', ['Optimal Ownership','Leverage'])
+            stacks_button = st.button('Get Stacks')
+            if stacks_button:
+                st.session_state['stack_data'] = controller.get_stacks(st.session_state['sim results'], stack_size)
+            st.dataframe(st.session_state['stack_data'])
+                
+    else: #no valid data
+        with data_tab:
+            st.subheader('Input data not available or format is incorrect. Upload data or confirm column names match the template')
+        with sims_tab:
+            st.subheader('Upload data to run sims')
+        with relationships_tab: 
+            st.subheader('Upload data and run sims to see player relationships')
+                   
 
-if st.session_state['sport'] != '':
+#%%NFL                
+if st.session_state['sport'] == 'NFL':
     intro_tab, data_tab, sims_tab, relationships_tab, correlations_tab = st.tabs(['What is this?','Import Projections','Run Sims', 'Explore Relationships', 'Edit Correlations'])
     
     #start appropriate class
-    if st.session_state['sport'] == 'NFL':
-        sport_class = sims.nfl()
-        st.session_state['correlation dict'] = {'QB':{'WR':.66,'TE':.33,'RB':.08, 'Opp_QB':.24}}
-    elif st.session_state['sport'] == 'MLB':
-        sport_class = sims.mlb()
-        st.session_state['correlation dict'] = {'SP':{}}
+    sport_class = sims.nfl()
+    st.session_state['correlation dict'] = {'QB':{'WR':.66,'TE':.33,'RB':.08, 'Opp_QB':.24}}
     
     #write documentation
     with intro_tab:
@@ -145,26 +247,24 @@ if st.session_state['sport'] != '':
             st.subheader('Upload data and run sims to see player relationships')
             
     #correlations section
-    #TODO: make dynamic
-    if st.session_state['sport'] == 'NFL':
-        with correlations_tab:
-            corr1,corr2,corr3,corr4 = st.columns(4)
-            with corr1:
-                st.session_state['correlation dict']['QB']['RB'] = st.number_input('QB - RB', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['RB'])
-            with corr2:
-                st.session_state['correlation dict']['QB']['WR'] = st.number_input('QB - WR', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['WR'])
-            with corr3:
-                st.session_state['correlation dict']['QB']['TE'] = st.number_input('QB - TE', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['TE'])
-            with corr4:
-                st.session_state['correlation dict']['QB']['Opp_QB'] = st.number_input('QB - Opposing QB', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['Opp_QB'])
-            reset_correlations = st.button('Reset')
-            if reset_correlations:
-                st.session_state['correlation dict'] = controller.get_default_correlations()
-                corr_df = controller.parse_correlation_to_df(st.session_state['correlation dict'])
-                st.table(corr_df.style.background_gradient())
-            else:
-                corr_df = controller.parse_correlation_to_df(st.session_state['correlation dict'])
-                st.table(corr_df.style.background_gradient())       
+    with correlations_tab:
+        corr1,corr2,corr3,corr4 = st.columns(4)
+        with corr1:
+            st.session_state['correlation dict']['QB']['RB'] = st.number_input('QB - RB', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['RB'])
+        with corr2:
+            st.session_state['correlation dict']['QB']['WR'] = st.number_input('QB - WR', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['WR'])
+        with corr3:
+            st.session_state['correlation dict']['QB']['TE'] = st.number_input('QB - TE', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['TE'])
+        with corr4:
+            st.session_state['correlation dict']['QB']['Opp_QB'] = st.number_input('QB - Opposing QB', min_value = -1.0, max_value = 1.0, value = st.session_state['correlation dict']['QB']['Opp_QB'])
+        reset_correlations = st.button('Reset')
+        if reset_correlations:
+            st.session_state['correlation dict'] = controller.get_default_correlations()
+            corr_df = controller.parse_correlation_to_df(st.session_state['correlation dict'])
+            st.table(corr_df.style.background_gradient())
+        else:
+            corr_df = controller.parse_correlation_to_df(st.session_state['correlation dict'])
+            st.table(corr_df.style.background_gradient())       
     
     #%%components and logic    
     with intro_container:
@@ -186,4 +286,4 @@ if st.session_state['sport'] != '':
                      ''')
                      
         
-    
+  
