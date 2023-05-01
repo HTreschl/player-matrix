dda49fd66463ee4f1c637efab5b84c46853280e3
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-05-01 15:43:11 -0500

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -147,7 +147,7 @@ if st.session_state['sport'] == 'MLB':
             st.subheader('Top Sims Lineups')
             lineups = controller.parse_lineups(st.session_state['lineups'])
             count = int(st.number_input('Number of Lineups to Export'))
-            stacks_to_include = st.multiselect('Stacks to Include', lineups['Summary'])
+            stacks_to_include = st.multiselect('Stacks to Include', lineups['Summary'].drop_duplicates())
             filtered_lineups = lineups[lineups['Summary'].isin(stacks_to_include)].sort_values(by = ['Lineup Score'], ascending=False).head(count)
             st.dataframe(filtered_lineups)
                 
