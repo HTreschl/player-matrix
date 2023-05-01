3410eff277e8d2a0541c1513a48cf2652050c08e
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-05-01 15:41:47 -0500

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -148,7 +148,7 @@ if st.session_state['sport'] == 'MLB':
             lineups = controller.parse_lineups(st.session_state['lineups'])
             count = int(st.number_input('Number of Lineups to Export'))
             stacks_to_include = st.multiselect('Stacks to Include', lineups['Summary'])
-            filtered_lineups = lineups[lineups['Sumary'].isin(stacks_to_include)].sort_values(by = ['Lineup Score'], ascending=False).head(count)
+            filtered_lineups = lineups[lineups['Summary'].isin(stacks_to_include)].sort_values(by = ['Lineup Score'], ascending=False).head(count)
             st.dataframe(filtered_lineups)
                 
     else: #no valid data
