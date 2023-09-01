cb5e06f5176c763593b83d7401700bd808c505db
HTreschl <85589267+HTreschl@users.noreply.github.com>
2023-08-31 20:29:08 -0500

    Update playerMatrixDashboard.py

--- playerMatrixDashboard.py
+++ playerMatrixDashboard.py
@@ -231,7 +231,13 @@ if st.session_state['sport'] == 'NFL':
             else:
                 ownership_col = None
                 
-            sims_results,lineups = sport_class.standard_sims(st.session_state['input data'], count,correlation_values = st.session_state['correlation dict'], fpts_col_name='Fpts', ceil_column = 'Ceil', floor_column = 'Floor',ownership_column=ownership_col, status_bar=status_bar)
+            sims_results,lineups = sport_class.standard_sims(st.session_state['input data'],
+                                                             count,
+                                                             fpts_col_name='Fpts',
+                                                             ceil_column = 'Ceil',
+                                                             floor_column = 'Floor',
+                                                             ownership_column=ownership_col,
+                                                             status_bar=status_bar)
             status_bar.empty()
             st.session_state['sim results'] = sims_results
             st.session_state['lineups'] = lineups
